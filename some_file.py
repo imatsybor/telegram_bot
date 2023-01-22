db8b347f75233ba2b79dee255dd9b04c9793b9ae
Igor Matsybor <imatsybor@gmail.com>
2023-01-22 18:15:39 +0200

    modified some_file.py

--- some_file.py
+++ some_file.py
@@ -1,3 +1,21 @@
-print('This is the file from GitHub repository!')
+from dataclasses import dataclass
 
-print('This is local commit')
+
+@dataclass
+class DatabaseConfig:
+    db_host: str  # URL-адрес базы данных
+    db_user: str  # Username пользователя базы данных
+    db_password: str  # Пароль к базе данных
+    database: str  # Название базы данных
+
+
+@dataclass
+class TgBot:
+    token: str  # Токен для доступа к телеграм-боту
+    admin_ids: list[int]  # Список id администраторов бота
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    db: DatabaseConfig
